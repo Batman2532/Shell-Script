d1b27ead11b55af4636ffe5fa55a4ca3e4ad52a1
Batman2532 <73815402+Batman2532@users.noreply.github.com>
2021-04-07 14:55:13 +0530

    Added: calculation of part time wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,14 +2,19 @@
 
 isAbsent=0
 isPresent=1
+partTime=2
 wagePerHour=20
 fullDay=8
-attendance=$((RANDOM%2))
+halfDay=4
+attendance=$((RANDOM%3))
 if [ $attendance -eq 1 ]
 then
 	wage=$(( $wagePerHour * $fullDay ))
 	echo "Employee is Present and his/her salary is: " $wage
-	
+elif [ $attendance -eq 2 ]
+then
+	wage=$(( $wagePerHour * $halfDay ))
+	echo "Employee is Present and his/her salary is: " $wage
 else
-	echo Employee is Absent	
+	echo Employee is Absent
 fi
