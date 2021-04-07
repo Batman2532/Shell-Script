5656e47d4e760b9168ffc8c6856167616333ac2e
Batman2532 <73815402+Batman2532@users.noreply.github.com>
2021-04-07 14:33:15 +0530

    Added: Attendance check

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,2 +1,11 @@
-	
-echo Welcome to Employee Wage Computation Program On Master Branch
+#!/bin/bash 
+
+isAbsent=0
+isPresent=1
+attendance=$((RANDOM%2))
+if [ $attendance -eq 1 ]
+then
+	echo Employee is Present
+else
+	echo Employee is Absent	
+fi
