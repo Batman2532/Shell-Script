42efa15a299745d23957b6e3913c453d23b1f79f
Batman2532 <73815402+Batman2532@users.noreply.github.com>
2021-04-07 14:45:01 +0530

    Added: calculation of daily employee wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,10 +2,14 @@
 
 isAbsent=0
 isPresent=1
+wagePerHour=20
+fullDay=8
 attendance=$((RANDOM%2))
 if [ $attendance -eq 1 ]
 then
-	echo Employee is Present
+	wage=$(( $wagePerHour * $fullDay ))
+	echo "Employee is Present and his/her salary is: " $wage
+	
 else
 	echo Employee is Absent	
 fi
