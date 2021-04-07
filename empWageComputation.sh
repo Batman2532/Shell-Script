492dd5a3f8181b898f67500b59c74cdb7e3629c1
Batman2532 <73815402+Batman2532@users.noreply.github.com>
2021-04-07 15:03:40 +0530

    Added: solved using switch case statement

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -7,14 +7,13 @@ wagePerHour=20
 fullDay=8
 halfDay=4
 attendance=$((RANDOM%3))
-if [ $attendance -eq 1 ]
-then
-	wage=$(( $wagePerHour * $fullDay ))
-	echo "Employee is Present and his/her salary is: " $wage
-elif [ $attendance -eq 2 ]
-then
-	wage=$(( $wagePerHour * $halfDay ))
-	echo "Employee is Present and his/her salary is: " $wage
-else
+case $attendance in
+	$isPresent)
+		wage=$(( $wagePerHour * $fullDay ))
+		echo "Employee is Present and his/her salary is: " $wage ;;
+	$partTime)
+		wage=$(( $wagePerHour * $halfDay ))
+		echo "Employee is Present and his/her salary is: " $wage ;;
+	*)
 	echo Employee is Absent
-fi
+esac
