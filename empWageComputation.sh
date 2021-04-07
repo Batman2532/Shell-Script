63e74eeaf436dc67aa6f831163a03aeb112fb183
Batman2532 <73815402+Batman2532@users.noreply.github.com>
2021-04-07 15:09:09 +0530

    Added: Calculating Wage for month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -6,13 +6,14 @@ partTime=2
 wagePerHour=20
 fullDay=8
 halfDay=4
+workingDay=20
 attendance=$((RANDOM%3))
 case $attendance in
 	$isPresent)
-		wage=$(( $wagePerHour * $fullDay ))
+		wage=$(( $wagePerHour * $fullDay * $workingDay ))
 		echo "Employee is Present and his/her salary is: " $wage ;;
 	$partTime)
-		wage=$(( $wagePerHour * $halfDay ))
+		wage=$(( $wagePerHour * $halfDay * $workingDay ))
 		echo "Employee is Present and his/her salary is: " $wage ;;
 	*)
 	echo Employee is Absent
