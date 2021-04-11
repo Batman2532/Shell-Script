8014193326d620e326e52aa5c812f240855240a5
Batman2532 <73815402+Batman2532@users.noreply.github.com>
2021-04-11 11:00:54 +0530

    Added: Function to get work hours

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -12,6 +12,10 @@ maxWorkingHr=100
 totalEmpHr=0
 totalWorkingDays=0
 
+function workHours() {
+	echo $totalEmpHr
+}
+
 while [ $totalEmpHr -le $maxWorkingHr ] | [ $totalWorkingDays -le $maxWorkingDays ]
 do
 	attendance=$((RANDOM%3))
@@ -20,12 +24,14 @@ case $attendance in
 	$isPresent)
 		((totalWorkingDays++))
 		totalEmpHr=$(($totalEmpHr + 8))
+		workHours $totalEmpHr
 		wage=$(( $wagePerHour * $fullDay * $maxWorkingDays ))
 		echo "Employee is Present and his/her salary is: " $wage ;;
 	$partTime)
 		((totalWorkingDays++))
 		totalEmpHr=$(($totalEmpHr + 4))
 		wage=$(( $wagePerHour * $halfDay * $maxWorkingDays ))
+		 workHours $totalEmpHr
 		echo "Employee is Present and his/her salary is: " $wage ;;
 	*)
 	echo Employee is Absent
